fdfc4e9e0813bd7586e535bb0f1a7d075368aed9
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-11-10 15:01:47 -0300

    feat: adiciona dados do profile no home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileService } from '../../services/profile.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProfileResponse } from '../../models/ProfileResponse';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleCasePipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
 
+  profile: ProfileResponse | null = null;
+  private profileService: ProfileService = inject(ProfileService);
   private access_token = localStorage.getItem("access_token");
 
   constructor(private navigator: Router) {}
@@ -17,6 +25,23 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     if (this.access_token == null) {
       this.navigator.navigate(['login']);
+      return;
     }
+
+    this.getProfile();
+  }
+
+  getProfile() {
+    this.profileService.getProfile().subscribe(
+      {
+        next: (response) => {
+          console.log(response);
+          this.profile = response;
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      }
+    );
   }
 }
