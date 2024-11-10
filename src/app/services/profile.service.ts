ed58af0db74cdbcd893622b1e1398dd680e4a59c
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-11-09 21:23:13 -0300

    feat: adiciona service profile (service que busca os dados de perfil do usuário)

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { ProfileResponse } from '../models/ProfileResponse';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  private readonly baseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.api;
+  }
+
+  getProfile(): Observable<ProfileResponse> {
+    return this.http.get<ProfileResponse>(this.baseUrl + 'api/v1/profile');
+  }
+}
