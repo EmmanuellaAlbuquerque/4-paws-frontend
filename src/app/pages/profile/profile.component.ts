21793d49e9e8858eebba779fc0024c06902465a7
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-11-18 17:15:23 -0300

    feat: exibindo todos os dados do profile na página profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile/profile.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProfileResponse } from '../../models/ProfileResponse';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +10,22 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  profile: ProfileResponse | null = null;
+  private profileService: ProfileService = inject(ProfileService);
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe(
+      {
+        next: (response) => {
+          this.profile = response;
+          console.log(this.profile);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      }
+    );
+  }
 }
