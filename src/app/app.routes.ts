95e97d9a39d7b70ff5b670977dbdd53c37015c69
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-11-25 19:49:33 -0300

    feat: adiciona página de criação de uma consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TutorComponent } from './pages/tutor/tutor.component';
 import { PetComponent } from './pages/pet/pet.component';
+import { AppointmentsFormComponent } from './pages/appointments/appointments-form/appointments-form.component';
 
 export const routes: Routes = [
   {
@@ -31,6 +32,10 @@ export const routes: Routes = [
   {
     path: 'pets',
     component: PetComponent
+  },
+  {
+    path: 'appointments',
+    component: AppointmentsFormComponent
   },
   //** THE ORDER MATTERS **(ALL OTHER ROUTES) SHOULD BE AT THE END **//
   {
